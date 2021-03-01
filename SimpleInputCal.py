4b458308c76710fc71a605437f8845a7fd369153
Manideep Devu <devu.manideep@gmail.com>
2021-03-01 11:46:37 +0530

    Added Different Cal

--- SimpleInputCal.py
+++ SimpleInputCal.py
@@ -2,3 +2,5 @@ num1 = int(input("Enter Number 1:"))
 num2 = int(input("Enter Number 2: "))
 sum = num1 + num2
 print('Sum is :'+str(sum))
+print('Diference is:'+str(num1-num2))
+print('Multiplied '+str(num1*num2))
