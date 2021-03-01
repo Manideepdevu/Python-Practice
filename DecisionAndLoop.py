12b2699c3edfe697628dcd588aa801fde6a4b49d
Manideep Devu <devu.manideep@gmail.com>
2021-03-01 15:21:32 +0530

    Added the loops

--- DecisionAndLoop.py
+++ DecisionAndLoop.py
@@ -3,8 +3,14 @@ num2 = int(input("Enter Last Number:"))
 num3 = range(num1,num2)
 
 for index,number in enumerate(num3,start = 1):
+  print("testing for loop")
   if number == 0:
    print("U started wtih zero")
 
   else:
    print(index,'#',number)
+
+while (num1<num2):
+    print("now its to test while")
+    print("reaching num2 slowly ",num1)
+    num1 = num1 +1
